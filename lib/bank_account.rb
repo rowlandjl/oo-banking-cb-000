15211f1bacb1cbe1222c07c925dcf9e24bac0b7e
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 00:45:05 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,5 +6,17 @@ class BankAccount
     @balance = 1000 
     @status = "open"
   end 
+  
+  def deposit 
+  end 
+  
+  def display_balance 
+  end 
+  
+  def valid?
+  end 
+  
+  def close_account 
+  end 
 
 end
