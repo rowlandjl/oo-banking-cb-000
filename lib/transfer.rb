554555803502df298b8e7553e3e40a0a04917e18
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 01:02:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,6 +15,7 @@ class Transfer
   def execute_transaction 
     if self.status == "pending" && self.sender.balance >= self.amount
       self.sender.balance -= self.amount 
+      self.receiver.balance += self.amount 
   end 
   
   def reverse_transfer 
